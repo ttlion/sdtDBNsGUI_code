8fac975aafc642392bf701aa3e0c3e2afa9cf06f
ttlion <tiago.miguel.leao@gmail.com>
2020-03-15 16:04:16 +0000

    changes to menu to hyperlink to webpages and repos

--- mainMenu/Menu.py
+++ mainMenu/Menu.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import sys
+import webbrowser
 
 #########################################
 # Menu bar class
@@ -9,9 +10,11 @@ class MyMenu:
     def __init__(self, frame):
         self.menubar = Menu(frame)
         self.filemenu = Menu(self.menubar, tearoff=0)
-        self.filemenu.add_command(label="New", command=self.donothing)
-        self.filemenu.add_command(label="Open", command=self.donothing)
-        self.filemenu.add_command(label="Save", command=self.donothing)
+
+        # Uncomment this to add options to "File" submenu
+        # self.filemenu.add_command(label="New", command=self.donothing)
+        # self.filemenu.add_command(label="Open", command=self.donothing)
+        # self.filemenu.add_command(label="Save", command=self.donothing)
 
         self.filemenu.add_separator()
 
@@ -19,14 +22,32 @@ class MyMenu:
 
         self.menubar.add_cascade(label="File", menu=self.filemenu)
 
-        self.editmenu = Menu(self.menubar, tearoff=0)
+        self.helpmenu = Menu(self.menubar, tearoff=0)
         
-        # edit submenu
-        self.editmenu.add_command(label="Undo", command=self.donothing)
-        self.menubar.add_cascade(label="Edit", menu=self.editmenu)
+        # "Help" submenu
+        self.helpmenu.add_command(label="sdtDBNs webpage", command=self.gotoWebpage_sdtDBN)
+        self.helpmenu.add_command(label="sdtDBNs Github project", command=self.gotoRepo_sdtDBN)
+
+        self.helpmenu.add_command(label="sdtDBNs GUI webpage", command=self.gotoWebpage_GUI)
+        self.helpmenu.add_command(label="sdtDBNs GUI Github project", command=self.gotoRepo_GUI)
+
+        self.menubar.add_cascade(label="Help", menu=self.helpmenu)
 
         frame.config(menu=self.menubar)
 
-    def donothing(self):
+    def gotoWebpage_sdtDBN(self):
+        webbrowser.open_new('https://ttlion.github.io/sdtDBN/')
+        return
+
+    def gotoWebpage_GUI(self):
+        webbrowser.open_new('https://ttlion.github.io/sdtDBN/')
+        return
+    
+    def gotoRepo_sdtDBN(self):
+        webbrowser.open_new('https://github.com/ttlion/sdtDBN_code')
+        return
+    
+    def gotoRepo_GUI(self):
+        webbrowser.open_new('https://github.com/ttlion/sdtDBNsGUI')
         return
     
